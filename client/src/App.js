a6cc763c5779374c73e6e9472d8572430db72715
Pedja-Djape <pmuratov1015@gmail.com>
2022-12-20 18:21:56 -0500

    confirm effective comms b/w client and backend

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [apiResponse,setAPIResponse] = useState('');
 
-  const gback = () => {
-    fetch("http://localhost:9000/testAPI").then(
-      res => res.text()
-    ).then(
-      res => setAPIResponse(res)
-    );
-  }
-  gback();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{apiResponse}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
