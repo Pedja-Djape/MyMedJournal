289395d309a54d94d08744e44e42dc82fcafe798
Pedja-Djape <pmuratov1015@gmail.com>
2022-12-26 16:56:59 -0500

    Added Styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import classes from './App.module.css'
+
+import { Routes, Route } from 'react-router-dom';
+
+import Navbar from "./components/UI/Navbar";
+import Home from "./pages/Home.js";
+import Search from "./pages/Search";
+import About from "./pages/About";
+import NoPage from "./pages/NoPage";
+
+
 
 function App() {
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const pages = {
+		"search": 'Search',
+		'about': 'About'
+	}
+
+
+	return (
+	<React.Fragment>
+		<Navbar pages={pages}/>
+		<div className={classes.container}>
+			<Routes> {/*List of Routes*/}
+				<Route path='/' element={<Home />} />
+				<Route path="search" element={<Search />} />
+				<Route path="about" element={<About />} />
+				<Route path="*" element={<NoPage />} />
+			</Routes>
+		</div>
+	</React.Fragment>
+
+	);
 }
 
 export default App;
