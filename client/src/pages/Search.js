d158798242dc13ba5ab5d7df519f1cdc8d9ceb26
Pedja-Djape <pmuratov1015@gmail.com>
2022-12-26 20:31:26 -0500

    removed max-width setting

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,5 +1,39 @@
+import { useState } from "react";
+
+import SearchBar from "../components/SearchTools/SearchBar";
+import classes from './Search.module.css';
+
 const Search = () => {
-    return <h1>Search</h1>;
+
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const searchHandler = (enteredValue) => {
+        setSearchQuery(enteredValue);
+    }
+
+    return (
+        <div className={classes.container}>
+            {/* <div className={classes.pageTitle}>
+                <h1>Search</h1>
+            </div>
+            <div className={classes.searchBar}>
+                <div>
+                    <SearchBar onSetQuery={searchHandler}/>
+                </div>
+            </div> */}
+            <div className={classes.pageTitle}>
+                <h1>Search for medical journals!</h1>
+            </div>
+            <div className={classes['flex-container']}>
+                <div className={classes['search-bar']}>
+                    <SearchBar onSetQuery={searchHandler} />
+                </div>
+                <div className={classes.e1}></div>
+                <div className={classes.e1}></div>
+            </div>
+        </div>
+    
+    );
 };
   
 export default Search;
