6e40ef8734598cde68c82ed97a871f8d7667e4ee
Pedja-Djape <pmuratov1015@gmail.com>
2022-12-27 00:02:20 -0500

    added api call to backend for search

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,36 +1,63 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 import SearchBar from "../components/SearchTools/SearchBar";
 import classes from './Search.module.css';
+import Card from "../components/UI/Card";
+
 
 const Search = () => {
 
     const [searchQuery, setSearchQuery] = useState('');
+    const [articlesData, setArticlesData] = useState([]);
+
+    
 
     const searchHandler = (enteredValue) => {
+        // replace all whitespace with space
         setSearchQuery(enteredValue);
-    }
+        
+        const path = "/papers/search?" + new URLSearchParams({
+            db: "pubmed",
+            term: enteredValue
+        });
+        fetch(path)
+            .then( (res) => res.json())
+            .then( (data) => {
+                if (data.code === 200){
+                    setArticlesData(data.data);
+                }
+            }); 
+    } 
+
+    
+    
+    
 
     return (
         <div className={classes.container}>
-            {/* <div className={classes.pageTitle}>
-                <h1>Search</h1>
-            </div>
-            <div className={classes.searchBar}>
-                <div>
-                    <SearchBar onSetQuery={searchHandler}/>
-                </div>
-            </div> */}
             <div className={classes.pageTitle}>
-                <h1>Search for medical journals!</h1>
+                <h1>Search for medical articles!</h1>
             </div>
             <div className={classes['flex-container']}>
                 <div className={classes['search-bar']}>
                     <SearchBar onSetQuery={searchHandler} />
                 </div>
-                <div className={classes.e1}></div>
-                <div className={classes.e1}></div>
             </div>
+            <div className={classes['cards-container']}>
+                {
+                    articlesData.length > 0 ?
+                        articlesData.map( (article) => 
+                        <div key={article.id} className={classes.card}>
+                            <Card 
+                                title={article.title}
+                                body={article.abstract}
+                            />
+                        </div>
+                        ) :
+                        null
+                }
+            </div>
+            
+            
         </div>
     
     );
