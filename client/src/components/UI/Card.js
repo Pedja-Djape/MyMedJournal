134ec793d6070aa86a422c1ce88b008480b6f6e3
Pedja-Djape <pmuratov1015@gmail.com>
2023-01-06 12:38:16 -0500

    changed style to allow multi-line truncation

--- client/src/components/UI/Card.js
+++ client/src/components/UI/Card.js
@@ -5,27 +5,16 @@ import { useState } from 'react';
 
 const Card = (props) => {
     
-    const mainText = props.body.slice(0,300);
-    const remainingText = props.body.slice(300,props.body.length);
-
-    const [textBody, setTextBody] = useState(mainText + '...');
-
-    
-
-    const onSeeMore = () => {
-        setTextBody(props.body);
-    }
-
     return <>
-        <div className={classes.card}>
+        <div className={classes.card} style={{'background-color': props.color}}>
             <div className={classes['card-header']}>
                 {props.title}
             </div>
             <div className={classes['card-body']}>
-                {textBody}
+                {props.body}
             </div>
             <div className={classes['card-footer']}>
-                <button type='button' onClick={onSeeMore}>See More.</button>
+                <button type='button'>See More.</button>
             </div> 
         </div>
     </>
