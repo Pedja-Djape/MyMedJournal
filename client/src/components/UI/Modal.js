276329e7b6b87811050f1c9a53ad7e650cfc62b0
Pedja-Djape <pmuratov1015@gmail.com>
2023-01-07 17:43:24 -0500

    updated modal

--- client/src/components/UI/Modal.js
+++ client/src/components/UI/Modal.js
@@ -1,32 +1,15 @@
-import ReactDom from 'react-dom';
 
-const MODAL_STYLE = {
-    position: 'fixed',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%,-50%)',
-    padding: '50px',
-    zIndex: 1000
-}
+import { useEffect, useRef } from "react";import ReactDom from 'react-dom';
+import classes from './Modal.module.css';
 
-const OVERLAY_STYLES = {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: 'rgba(0,0,0,.7)',
-    zIndex: 1000
-}
 
 const Modal = (props) => {
-    if (!props.open) return null;
+    // if (!props.open) return null;
 
     return ReactDom.createPortal(
         <>
-            <div style={OVERLAY_STYLES}>
-                <div style={MODAL_STYLE}>
-                    <button type="button" onClick={props.onModalClose}>Close</button>
+            <div className={classes.overlay}>
+                <div className={classes.modal}>
                     {props.children}
                 </div>
             </div>
