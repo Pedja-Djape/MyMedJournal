50b3199b994dbd1da45a5bdb1db0c0020fe487c5
Pedja-Djape <pmuratov1015@gmail.com>
2023-05-24 20:53:31 -0400

    converted css to tailwindcss

--- client/src/components/SearchTools/SearchBar.js
+++ client/src/components/SearchTools/SearchBar.js
@@ -1,4 +1,3 @@
-import classes from './SearchBar.module.css';
 import searchIcon from '../../images/search.png';
 
 import { useState } from 'react';
@@ -27,14 +26,18 @@ const SearchBar = (props) => {
 
     return (
         <div>
-            <form onSubmit={formSubmitHandler} className={classes.searchBar}>
+            <form onSubmit={formSubmitHandler} className="w-full bg-[rgba(255,255,255,0.2)] flex items-center rounded-3xl px-4 py-2 backdrop-blur-sm backdrop-saturate-200">
                 <input 
                     type="text" 
                     placeholder='Search Query.' 
                     onChange={inputChangeHandler}
+                    className=' bg-transparent flex-1 border-0 outline-none px-[10px] py-[20px] text-lg text-[cac7ff]'
                 />
 
-                <button type="submit"><img src={searchIcon}/></button>
+                <button type="submit" className='border-0 rounded-[50%] w-[60px] h-[60px] bg-[#58629b] cursor-pointer flex items-center justify-center'>
+                    <img src={searchIcon} className='w-[25px]'/>
+                    
+                </button>
             </form>
         </div>
     );
