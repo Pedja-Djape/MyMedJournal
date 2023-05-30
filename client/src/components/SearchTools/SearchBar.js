9e2dcf97a4a2c51ad66d8ce5e329246ef2498828
Pedja-Djape <pmuratov1015@gmail.com>
2023-05-30 18:19:45 -0400

    modified code to do nothing on empty submit

--- client/src/components/SearchTools/SearchBar.js
+++ client/src/components/SearchTools/SearchBar.js
@@ -1,41 +1,32 @@
 import searchIcon from '../../images/search.png';
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 const SearchBar = (props) => {
 
-    const [enteredValue, setEnteredValue] = useState('');
-    const [isValid, setIsValid] = useState(true);
+    const searchQuery = useRef(null);
 
-    const inputChangeHandler = event => {
-        if (event.target.value.trim().length > 0) {
-            setIsValid(true);
-        }
-        setEnteredValue(event.target.value);
-    };
 
     const formSubmitHandler = event => {
         event.preventDefault();
-        if (enteredValue.trim().length === 0) {
-            setIsValid(false);
+        if (!searchQuery.current.value || searchQuery.current.value.trim().length === 0) {
             return;
         }
-        props.onSetQuery(enteredValue);
+        props.onSetQuery(searchQuery.current.value);
     }
-
 
     return (
         <div>
-            <form onSubmit={formSubmitHandler} className="w-full bg-[rgba(255,255,255,0.2)] flex items-center rounded-3xl px-4 py-2 backdrop-blur-sm backdrop-saturate-200">
+            <form onSubmit={formSubmitHandler} className="w-full bg-[rgba(255,255,255,0.2)] flex items-center rounded-3xl px-4 py-2 backdrop-blur-sm ">
                 <input 
                     type="text" 
-                    placeholder='Search Query.' 
-                    onChange={inputChangeHandler}
-                    className=' bg-transparent flex-1 border-0 outline-none px-[10px] py-[20px] text-lg text-[cac7ff]'
+                    placeholder='Search keywords.' 
+                    ref={searchQuery}
+                    className=' bg-transparent flex-1 border-0 outline-none px-[10px] py-[20px] text-lg text-white'
                 />
 
-                <button type="submit" className='border-0 rounded-[50%] w-[60px] h-[60px] bg-[#58629b] cursor-pointer flex items-center justify-center'>
-                    <img src={searchIcon} className='w-[25px]'/>
+                <button type="submit" className='border-0 rounded-[50%] w-[60px] h-[60px] bg-[#100e0e] cursor-pointer flex items-center justify-center'>
+                    <img src={searchIcon} alt='search-icon' className='w-[25px]'/>
                     
                 </button>
             </form>
