3a9f056bb863be213127bb11f621c6f20a577e5a
Pedja-Djape <pmuratov1015@gmail.com>
2023-05-30 17:32:01 -0400

    used tailwind with standard css to customize navbar

--- client/src/components/UI/MainNavigation.js
+++ client/src/components/UI/MainNavigation.js
@@ -2,24 +2,42 @@ import { NavLink } from "react-router-dom";
 
 import React from 'react'
 
+import classes from './MainNavigation.module.css';
+
 const MainNavigation = () => {
 
-
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to='/' end>
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/search'>
-                            Search
-                        </NavLink>
-                    </li>
-                </ul>
+        <header className="w-full p-4 bg-black text-white">
+            <nav className="flex justify-between items-center">
+                <div className="flex text-2xl">
+                    <NavLink 
+                        to='/' 
+                        end
+                    >
+                        MyMedJournal
+                    </NavLink>
+                </div>
+                <div>
+                    <ul className={classes.list}>
+                        <li>
+                            <NavLink 
+                                to='/' 
+                                className={({ isActive }) => isActive ? classes.active : undefined }
+                            >
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="text-white">
+                            <NavLink 
+                                to='/search'
+                                className={({ isActive }) => isActive ? classes.active : undefined }
+                            >
+                                Search
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+                
             </nav>
         </header>
   )
