d611cdaae8b7c98ba813eeacb028c705a5744e67
Pedja-Djape <pmuratov1015@gmail.com>
2023-05-31 17:40:24 -0400

    completed initial authentication action

---
+++ client/src/pages/Authentication.js
@@ -0,0 +1,48 @@
+import { json, redirect } from 'react-router-dom'
+
+import AuthForm from "../components/AuthForm";
+
+const AuthenticationPage = () => {
+    return <AuthForm />
+}
+
+export const action = async ({request}) => {
+    // get search parameters from route
+    const searchParams = new URL(request.url).searchParams;
+    // creating account or logging in?
+    const mode = searchParams.get('mode') || 'login';
+    
+    // get email and password
+    const data = await request.formData();
+    const authData = {
+        email: data.get('email'),
+        password: data.get('password')
+    };
+
+    // tell backend to create request
+    const response = await fetch('http://localhost:8080/' + mode, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(authData)
+    });
+
+    if (response.status === 422 || response.status === 401) {
+        return response;
+    }
+
+    if (!response.ok) {
+        throw json({ message: 'Could not authenticate user.'}, {status: 500});
+    }
+
+    const resData = await response.json();
+    const token = resData.token;
+    // temporarily store in localStorage
+    localStorage.setItem('token', token);
+}
+
+
+
+
+export default AuthenticationPage;
