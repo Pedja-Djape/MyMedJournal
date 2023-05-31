20ca682e5cdaab7ee36f3155dd515caada59b8ea
Pedja-Djape <pmuratov1015@gmail.com>
2023-05-31 09:19:01 -0400

    added AuthForm template code

---
+++ client/src/components/AuthForm.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { 
+    Form,
+    Link,
+    useSearchParams,
+    useActionData,
+    useNavigation
+} from 'react-router-dom';
+
+import classes from './AuthForm.module.css';
+
+const AuthForm = () => {
+
+    // const data = useActionData(); 
+    const data = null;
+    // const navigation = useNavigation()
+
+    const [ searchParams ] = useSearchParams();
+
+    const isLogin = searchParams.get('mode') === 'login';
+    // const isSubmitting = navigation.state === 'submitting';
+
+    return (
+        <>
+        <Form method="post" className={classes.form}>
+            <h1>{isLogin ? 'Log in' : 'Create an account.'}</h1>
+                {data && data.errors && (
+                    <ul>
+                        {Object.values(data.errors).map((err) => (
+                            <li key={err}>{err}</li>
+                        ))}
+                    </ul>
+                )}
+            {data && data.message && <p>{data.message}</p>}
+            <p>
+                <label htmlFor="email">Email</label>
+                <input id="email" type="email" name="email" required />
+            </p>
+            <label htmlFor="image">Password</label>
+
+            <input id="password" type="password" name="password"  required />
+           
+            <div className={classes.actions}>
+                <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
+                    {isLogin ? 'Create an account.' : 'Login.'}
+                </Link>
+                <button disabled={false}>   
+                    Save
+                </button>
+            </div>
+        </Form>
+        </>
+    )
+}
+
+export default AuthForm
