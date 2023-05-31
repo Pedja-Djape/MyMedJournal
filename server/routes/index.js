7c38b9ea4e7eeb1606f4b4d0db2a1dd88d6e8303
Pedja-Djape <pmuratov1015@gmail.com>
2023-05-31 09:31:20 -0400

    added temp route /yo

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/yo', (req, res, next) => {
+  res.json({message: 'Hello, from the backend!'})
+})
+
+
 module.exports = router;
