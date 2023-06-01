c9389977109874935f8930a4aff9f196d211e49f
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-01 18:58:10 -0400

    redirect user to dashboard after successful authentication

--- client/src/components/AuthForm.js
+++ client/src/components/AuthForm.js
@@ -42,7 +42,7 @@ const AuthForm = () => {
                 id: userId
             }
             dispatch(authActions.manageToken(payload));
-            navigate("/");
+            navigate("/dashboard");
             return;
         }
     }
