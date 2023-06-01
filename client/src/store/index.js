53adce59d0157a9d9117137ba730705607388b2b
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-01 10:45:04 -0400

    modified slice name to authSlice

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import uiSlice from "./uiSlice";
+import authSlice from "./authSlice";
 
 const store = configureStore({
     reducer: {
-        ui: uiSlice.reducer
+        auth: authSlice.reducer
     }
 });
 
