0a957ddf65869615d87899b92db11e9350987375
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-01 18:53:59 -0400

    added component to render list of notes

---
+++ client/src/components/NotesList.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import NoteCard from './NoteCard'
+
+const NotesList = ({notes}) => {
+    return (
+        <div className="flex flex-col items-center justify-center ">
+            <div className="text-4xl text-center mb-8">
+                <h1 className='text-white'>Your notes.</h1>
+            </div>
+            <div className="flex flex-wrap gap-8 w-full">
+                {
+                    notes.map((note) => (
+                        <Link>
+                            <NoteCard title={note.title} content={note.content}/>
+                        </Link>
+                    ))
+                }
+            </div>
+        </div>
+        
+    )
+}
+
+export default NotesList
