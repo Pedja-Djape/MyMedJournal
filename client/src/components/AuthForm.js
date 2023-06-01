9d6e84ea1b6a6bd373458620468907c88a09409d
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-01 15:43:49 -0400

    added client-side validation on credentials

--- client/src/components/AuthForm.js
+++ client/src/components/AuthForm.js
@@ -4,17 +4,26 @@ import {
     Link,
     useSearchParams,
     useActionData,
-    useNavigate
+    useNavigate,
+    useNavigation
 } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 import { useDispatch } from 'react-redux';
 import { authActions } from '../store/authSlice';
+import useInput from '../hooks/useInput';
+
+const invalidStyling = {
+    border: '1px solid #b40e0e',
+    backgroundColor: '#d1b6b6'
+}
+
 
 let isFirst = true;
 
 const AuthForm = () => {
     const navigate = useNavigate();
+    const navigation = useNavigation();
     const data = useActionData();
     const dispatch = useDispatch();
 
@@ -37,10 +46,31 @@ const AuthForm = () => {
     }
     , [token, navigate, dispatch]);
 
+    const {
+        value: enteredEmail,
+        hasError: emailHasError,
+        isValid: emailIsValid,
+        valueChangeHandler: emailChangedHandler,
+        inputBlurHandler: emailBlurHandler,
+        reset: resetEmail
+    } = useInput( (email) => email.includes('@'));
+
+    const {
+        value: enteredPassword,
+        hasError: passwordHasError,
+        isValid: passwordIsValid,
+        valueChangeHandler: passwordChangedHandler,
+        inputBlurHandler: passwordBlurHandler,
+        reset: resetPassword
+    } = useInput( (password) => password.length > 7);
+
+
     const [ searchParams ] = useSearchParams();
 
     const isLogin = searchParams.get('mode') === 'login';
-
+    // to prevent from multiple successive requests during submission
+    const isSubmitting = navigation.state === 'submitting'
+    
     return (
         <>
         <Form method="post" className={classes.form}>
@@ -53,20 +83,36 @@ const AuthForm = () => {
                     </ul>
                 )}
             {data && data.message && <p>{data.message}</p>}
-            <p>
                 <label htmlFor="email">Email</label>
-                <input id="email" type="email" name="email" required />
-            </p>
+                <input 
+                    id="email" 
+                    type="email" 
+                    name="email" 
+                    onChange={emailChangedHandler}
+                    onBlur={emailBlurHandler}
+                    value={enteredEmail}
+                    style={emailHasError ? invalidStyling : null}
+                    required 
+                />
+            {emailHasError && <p className='error-text'>Entered email must be a valid email address!</p>}
             <label htmlFor="image">Password</label>
-
-            <input id="password" type="password" name="password" required  />
-           
+            <input 
+                id="password" 
+                type="password" 
+                name="password" 
+                value={enteredPassword} 
+                onChange={passwordChangedHandler}
+                onBlur={passwordBlurHandler}
+                style={passwordHasError ? invalidStyling : null}
+                required  
+            />
+            {passwordHasError && <p className='error-text'>Enteted password must be at least 8 characters long!</p>}
             <div className={classes.actions}>
                 <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
                     {isLogin ? 'Create an account.' : 'Login.'}
                 </Link>
-                <button disabled={false}>   
-                    Save
+                <button disabled={isSubmitting || !emailIsValid || !passwordIsValid}>   
+                    {isSubmitting ? 'Submitting ...' : 'Submit.'}
                 </button>
             </div>
         </Form>
