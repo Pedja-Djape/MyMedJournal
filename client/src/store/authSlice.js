0d1a3fcf3dc487e27fa1ab510d634653ff537b4b
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-01 14:47:08 -0400

    added email and id fields

--- client/src/store/authSlice.js
+++ client/src/store/authSlice.js
@@ -4,12 +4,16 @@ const authSlice = createSlice({
     name: 'auth',
     initialState: {
         isAuthenticated: false,
-        token: null
+        token: null,
+        email: null,
+        id: null
     },
     reducers: {
         manageToken(state, action) {
             state.isAuthenticated = action.payload.isAuthenticated;
             state.token = action.payload.token;
+            state.email = action.payload.email;
+            state.id = action.payload.email;
         }
     }
 })
