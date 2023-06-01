f19540d8cd6c5dae97a9fc1782b200dc9f01a1ac
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-01 17:29:21 -0400

    using one validCredentials function

--- server/util/validateCreds.js
+++ server/util/validateCreds.js
@@ -7,7 +7,18 @@ function validatePassword(password) {
     return password.length > 7;
 }
 
+function validateCredentials(email, password) {
+    let errors = {}
+    if (!validateEmail(email)){ 
+        errors.email = "Invalid email!";
+    }
+
+    if (!validatePassword(password)) {
+        errors.password = "Invalid password!";
+    }
+    return errors
+}
+
 module.exports = {
-    validateEmail,
-    validatePassword
+    validateCredentials
 }
