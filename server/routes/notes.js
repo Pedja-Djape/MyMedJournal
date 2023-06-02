4bfab044555df7503a6e3150474c5d3bd6ac1249
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-02 13:41:10 -0400

    added init routes to get and add routes

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,5 +1,64 @@
 const express = require('express');
+const Notes = require('../models/note.model');
+const { default: mongoose } = require('mongoose');
+
 const router = express.Router();
 
 
+router.get('/', async (req, res) => {
+    const userEmail = req.body.email;
+
+    const userNotes = await Notes.findOne({_id: userEmail}).exec();
+    console.log(userNotes)
+    return res.status(200).send({
+        message: "Successfully obtained notes.",
+        notes: userNotes.notes
+    })
+
+});
+
+// ADD AUTHENTICATIION AND VALIDATION!!!!
+router.post('/add', async (req, res) => {
+    // auth check with token (use middleware before this function is called)
+    // check if fields are empty
+    // check if userID exists
+    
+    const {userId, noteTitle, noteContent} = req.body;
+    // fetch users notes
+    const userNotes = await Notes.findOne({_id: userId}).exec();
+
+    // user has no notes
+    if (userNotes === null) {
+        const newUserNotes = new Notes({
+            _id: userId,
+            notes: [
+                {
+                    title: noteTitle,
+                    content: noteContent
+                }
+            ]
+        });
+        const result = await newUserNotes.save();
+        return res.status(200).send({
+            noteId: result.notes[0]._id.toString()
+        });
+    } else {
+        const newNote = {
+            title: noteTitle,
+            content: noteContent
+        }
+        const numNotes = userNotes.notes.push(newNote)
+        const result = await userNotes.save();
+        console.log(result);
+        return res.status(200).send(
+            {
+                noteId: userNotes.notes[numNotes - 1]._id.toString()
+            }
+        )
+    }
+
+})
+
+
+module.exports = router;
 
