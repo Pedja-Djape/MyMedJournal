ddb70ef8489ce1c0a41ee44afaa86e9f735eb461
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-02 11:34:38 -0400

    added routes for Notes

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import {
  import AuthenticationPage, {action as authAction} from './pages/Authentication';
 import DashboardLayout from "./pages/DashboardLayout";
 import NotesPage from "./pages/Notes";
+import NotesLayout from "./pages/NotesLayout";
+import NoteDetail from "./pages/NoteDetail";
  
 
 const router = createBrowserRouter([
@@ -41,7 +43,22 @@ const router = createBrowserRouter([
 					},
 					{
 						path: 'notes',
-						element: <NotesPage />
+						element: <NotesLayout/>,
+						children: [
+							{
+								index: true,
+								element: <NotesPage />
+							},
+							{
+								path: ":noteId",
+								children: [
+									{
+										index: true,
+										element: <NoteDetail />
+									}
+								]
+							}
+						]
 					}
 				]
 			},
