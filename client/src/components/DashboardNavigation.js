656c6df0708f9b3e68817740ba5742ba618a2c3f
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-02 17:56:17 -0400

    notes endpoint with user id

--- client/src/components/DashboardNavigation.js
+++ client/src/components/DashboardNavigation.js
@@ -1,13 +1,15 @@
 import { Sidebar, Menu, MenuItem,  } from 'react-pro-sidebar';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const DashboardNavigation = () => {
+  const uid = useSelector(state => state.auth.id);
   return (
     <>
     <div style={{paddingTop: '3rem'}}>
         <Sidebar>
                 <Menu>
-                    <MenuItem component={<Link to='' />}>No-Op</MenuItem>
+                    <MenuItem component={<Link to={`/dashboard/${uid}/notes`} />}>Notes</MenuItem>
                 </Menu>
             </Sidebar>
     </div>
