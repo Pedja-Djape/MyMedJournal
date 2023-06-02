a61d6c6dd7ae3e07277fb5348597a9df2f8412dc
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-02 12:14:01 -0400

    rendering Note details with  NoteItem comp

--- client/src/pages/NoteDetail.js
+++ client/src/pages/NoteDetail.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import NoteItem from '../components/NoteItem';
 
 const NoteDetail = () => {
-  return (
-    <div>NoteDetail</div>
-  )
+	return (
+		<>
+			<NoteItem 
+				note={
+					{
+						id: 0, 
+						title: 'My thoughts', 
+						description: 'Article'
+					}
+				}
+			/>
+		</>
+	)
 }
 
 export default NoteDetail
