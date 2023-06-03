78f79cce322698324ba3fa648f9a337a36bec891
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-03 14:11:36 -0400

    initial page for adding a new note

---
+++ client/src/pages/NewNote.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NoteForm from '../components/NoteForm'
+
+const NewNotePage = () => {
+  return (
+    <NoteForm method='POST'/>
+  )
+}
+
+export default NewNotePage
