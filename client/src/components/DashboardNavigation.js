b03d6ab3092cb6db2b3ca8b0cc6db3372b58b634
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-04 15:00:12 -0400

    modified path to notes endpoint

--- client/src/components/DashboardNavigation.js
+++ client/src/components/DashboardNavigation.js
@@ -1,15 +1,13 @@
 import { Sidebar, Menu, MenuItem,  } from 'react-pro-sidebar';
-import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const DashboardNavigation = () => {
-  const uid = useSelector(state => state.auth.id);
   return (
     <>
     <div style={{paddingTop: '3rem'}}>
         <Sidebar>
                 <Menu>
-                    <MenuItem component={<Link to={`/dashboard/${uid}/notes`} />}>Notes</MenuItem>
+                    <MenuItem component={<Link to={`/dashboard/notes`} />}>Notes</MenuItem>
                 </Menu>
             </Sidebar>
     </div>
