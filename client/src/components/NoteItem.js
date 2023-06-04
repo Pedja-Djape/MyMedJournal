bd3ff00d0d3b0859df4af44cbc552cce6e27d9d2
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-04 14:52:40 -0400

    No longer returning from delete handler

--- client/src/components/NoteItem.js
+++ client/src/components/NoteItem.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import { Link, redirect, useNavigate, useSubmit } from 'react-router-dom'
+import { Link, useNavigate, useSubmit } from 'react-router-dom'
 
 import classes from './NoteItem.module.css';
 
 const NoteItem = ({note}) => {
     const submit = useSubmit();
-    const navigate = useNavigate();
     const deleteHandler = () => {
-        const proceed = window.confirm(`Are you sure you want to delete note: ${note.title}`);
+        const proceed = window.confirm(`Are you sure you want to delete note: '${note.title}'`);
         if (proceed) {
             submit(null, {method: 'DELETE'}); // submit to action assoc with route
-            return navigate('..');
+            return;
         }
     }
-
+    console.log(note);
     return (
         <article className={classes.note}>
             <h1>{note.title}</h1>
