9b3d0a3c27cbd49f86d899218203be79328684b0
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-05 19:28:55 -0400

    added boarders to buttons

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom'
 const Dashboard = () => {
   return (
     <div className='flex flex-col items-center w-full pt-6'>
-        <h1>Welcome to your dashboard!</h1>
+        <h1>Hi, welcome to your dashboard!</h1>
         <div className='flex justify-center gap-8'>
             <Link to='/dashboard/notes'>
-                <div className='w-[30rem] h-[30rem] bg-[#f2c11d] text-center text-white rounded-xl relative'>
+                <div className='w-[30rem] h-[30rem] bg-[#f2c11d] text-center text-white rounded-xl relative border-black border-3'>
                     <h1 className=' absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>Notes</h1>
                 </div>
             </Link>
-            <Link to='/dashbpard/favorites'>
-                <div className='w-[30rem] h-[30rem] bg-[#f2c11d] text-center text-white rounded-xl relative'>
+            <Link to='/dashboard/favorites'>
+                <div className='w-[30rem] h-[30rem] bg-[#f2c11d] text-center text-white rounded-xl relative border-black border-3'>
                     <h1 className=' absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>Favorites</h1>
                 </div>
             </Link>
