9b1aa8c4275ba169250ac5aa3771c0e2e9f89338
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-05 10:53:50 -0400

    changed styles for article popup

--- client/src/components/ArticlePopup/ArticlePopup.js
+++ client/src/components/ArticlePopup/ArticlePopup.js
@@ -5,7 +5,7 @@ import classes from "./ArticlePopup.module.css";
 const ArticlePopup = (props) => {
     
     return (<>
-        <div className={classes.card} style={{'backgroundColor': props.color}}>
+        <div className={classes.card} >
             <div className={classes['card-header']}>
                 <a href={`https://pubmed.ncbi.nlm.nih.gov/${props.id}/`} target="_blank">{props.title}</a>
             </div>
@@ -14,11 +14,9 @@ const ArticlePopup = (props) => {
                 {props.body}
             </div>
             <div className={classes['card-footer']}>
-                <div>
-                    <button type='button' className={classes.button} onClick={props.onClose}>
-                        Close
-                    </button>
-                </div>
+                <button type='button' className={classes.button} onClick={props.onClose}>
+                    Close
+                </button>
             </div> 
 
         </div>
