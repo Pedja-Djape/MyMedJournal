6a367d6a0989d4216e460fe3a120624941714a44
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-05 15:23:41 -0400

    Added to favorites list

--- client/src/components/FavoritesList.js
+++ client/src/components/FavoritesList.js
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Card from './UI/Card'
+import Modal from './UI/Modal';
+import ArticlePopup from "../components/ArticlePopup/ArticlePopup";
 
 const FavoritesList = ({favs}) => {
-    
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [cardInfo, setCardInfo] = useState({});
+
     const buttonInfo = {
         method: "DELETE",
         title: "Remove from favorites",
     }
 
+    const cardClickHandler = (title,abstract,id) => {
+            setCardInfo({
+                'title': title,
+                'abstract': abstract,
+                'id': id
+            })
+            setIsModalOpen(true);
+    }
+
     return (
         <>
+        {
+            isModalOpen ?
+                <Modal open={isModalOpen} onModalClose={() => setIsModalOpen(false)}>
+                    <ArticlePopup 
+                        title={cardInfo.title} 
+                        body={`${cardInfo.abstract}`}
+                        onClose={() => setIsModalOpen(false)}
+                        id={cardInfo.id}
+                    />
+                </Modal> :
+            null
+            }
         <div className='flex flex-col items-center justify-center '>
             <h1>Your favorite articles</h1>
             <div className='flex justify-center flex-wrap gap-2'>
@@ -20,6 +45,7 @@ const FavoritesList = ({favs}) => {
                             body={fav.abstract}
                             id={fav.id}
                             buttonInfo={buttonInfo}
+                            onCardClick={cardClickHandler}
                         />
                     </div>
                     
