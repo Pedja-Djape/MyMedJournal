9832a5a71139d4503adee697cc16854556f7c49d
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-05 15:08:50 -0400

    changed so clicking only on see more opens modal

--- client/src/components/UI/Card.js
+++ client/src/components/UI/Card.js
@@ -29,7 +29,7 @@ const Card = (props) => {
     }
     
     return <>
-        <div className={classes.card} style={{'backgroundColor': props.color}} onClick={handleCardClick}>
+        <div className={classes.card} style={{'backgroundColor': props.color}} >
             <div className={classes['card-header']}>
                 {props.title}
             </div>
@@ -37,7 +37,7 @@ const Card = (props) => {
                 {props.body}
             </div>
             <div className={classes['card-footer']}>
-                <button type='button'>See More.</button>
+                <button onClick={handleCardClick} type='button'>See More.</button>
                 <div className={classes.fav}>
                     <button onClick={favHandler} type='button'>{buttonInfo.title}</button>
                 </div>
