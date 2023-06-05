3a9e3a1a0fbe8e025525c0880c341a730911a1e4
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-05 14:54:22 -0400

    added button info prop to control fav/unfav

--- client/src/components/UI/Card.js
+++ client/src/components/UI/Card.js
@@ -1,16 +1,32 @@
+import { useSelector } from "react-redux";
 import classes from "./Card.module.css";
+import { useNavigate } from "react-router-dom";
 
 
 
 const Card = (props) => {
 
-    
-    
+    const { buttonInfo } = props;
+    const authState = useSelector(state => state.auth);   
+    const navigate = useNavigate(); 
     const handleCardClick = () => {
         props.onCardClick(props.title,props.body,props.id);
     }
 
-
+    const favHandler = () => {
+        fetch("http://localhost:9000/papers", {
+            method: buttonInfo.method,
+            headers: {
+                "Authorization": "Bearer " + authState.token,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                articleId: props.id
+            })
+        });
+        navigate('.')
+        
+    }
     
     return <>
         <div className={classes.card} style={{'backgroundColor': props.color}} onClick={handleCardClick}>
@@ -23,7 +39,7 @@ const Card = (props) => {
             <div className={classes['card-footer']}>
                 <button type='button'>See More.</button>
                 <div className={classes.fav}>
-                    <button type='button'>Add to favorites.</button>
+                    <button onClick={favHandler} type='button'>{buttonInfo.title}</button>
                 </div>
             </div> 
         </div>
