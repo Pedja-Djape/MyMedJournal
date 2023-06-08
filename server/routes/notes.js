8e26d4d91b2d40fbf1afd9e0b7e4aac33034dc3b
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-07 20:27:38 -0400

    added notes routes validation

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -32,6 +32,12 @@ router.get('/', authenticateToken, async (req, res) => {
 
 router.get('/:noteId', authenticateToken , async (req, res) => {
     const uid = req.user.userId;
+    if (!req.params.hasOwnProperty('noteId') || req.params.noteId === null || req.params.noteId === undefined || req.params.noteId === '') {
+        return res.status(400).send({
+            message: "Error! Please specifiy a valid noteId.",
+            note: []
+        })
+    }
     const { noteId } = req.params;
     try {
         const userNotes = await getUserNotes(uid);
@@ -58,20 +64,40 @@ router.patch('/:noteId', authenticateToken, async (req, res) => {
     const uid  = req.user.userId
     const { title, content} = req.body;
     const {noteId} = req.params;
-    try {
-        await Notes.updateOne(
-            { _id: uid, 'notes._id': noteId },
-            {$set: {
-                'notes.$.title': title, 'notes.$.content': content
-            }}
-        )
+    if (title && content && noteId) {
+        try {
+            await Notes.updateOne(
+                { _id: uid, 'notes._id': noteId },
+                {$set: {
+                    'notes.$.title': title, 'notes.$.content': content
+                }}
+            )
+            return res.status(200).send({
+                message: "Sucessfully updated note."
+            })
+        } catch (error) {
+            return res.status(500).send({
+                message: "Failed to update not."
+            })
+        }
+    }
+    if (!title) {
         return res.status(200).send({
-            message: "Sucessfully updated note."
+            message: "Error! Please provide a valid title.",
+            title: title
         })
-    } catch (error) {
-        return res.status(500).send({
-            message: "Failed to update not."
+    }
+    if (!content) { 
+        return res.status(200).send({
+            message: "Error! Please provide valid note content.",
+            content: content
         })
+    }
+    if (!noteId) {
+        return res.status(200).send({
+            message: "Error! Please provide a valid note id.",
+            noteId: noteId
+        });
     }
     
 
@@ -84,44 +110,65 @@ router.post('/', authenticateToken, async (req, res) => {
     // add try {} ... catch {} blocks for validation errors.
     const { userId } = req.user
     const { title: noteTitle, content: noteContent} = req.body;
-    // fetch users notes
-    try {
-        const userNotes = await Notes.findOne({_id: userId}).exec();
+    if (userId && title && content) {
+        // fetch users notes
+        try {
+            const userNotes = await Notes.findOne({_id: userId}).exec();
 
-        // user has no notes
-        if (userNotes === null) {
-            const newUserNotes = new Notes({
-                _id: userId,
-                notes: [
-                    {
-                        title: noteTitle,
-                        content: noteContent
-                    }
-                ]
-            });
-            const result = await newUserNotes.save();
-            return res.status(200).send({
-                noteId: result.notes[0]._id.toString()
-            });
-        } else {
-            const newNote = {
-                title: noteTitle,
-                content: noteContent
-            }
-            const numNotes = userNotes.notes.push(newNote)
-            const result = await userNotes.save();
-            return res.status(200).send(
-                {
-                    noteId: userNotes.notes[numNotes - 1]._id.toString()
+            // user has no notes
+            if (userNotes === null) {
+                const newUserNotes = new Notes({
+                    _id: userId,
+                    notes: [
+                        {
+                            title: noteTitle,
+                            content: noteContent
+                        }
+                    ]
+                });
+                const result = await newUserNotes.save();
+                return res.status(200).send({
+                    noteId: result.notes[0]._id.toString()
+                });
+            } else {
+                const newNote = {
+                    title: noteTitle,
+                    content: noteContent
                 }
-            )
+                const numNotes = userNotes.notes.push(newNote)
+                const result = await userNotes.save();
+                return res.status(200).send(
+                    {
+                        noteId: userNotes.notes[numNotes - 1]._id.toString()
+                    }
+                )
+            }
+        } catch (error) {
+            res.status(500).send({
+                message: "Failed to add note.",
+                error
+            })
         }
-    } catch (error) {
-        res.status(500).send({
-            message: "Failed to add note.",
-            error
+    }
+    if (!title) {
+        return res.status(200).send({
+            message: "Error! Please provide a valid title.",
+            title: title
         })
     }
+    if (!content) { 
+        return res.status(200).send({
+            message: "Error! Please provide valid note content.",
+            content: content
+        })
+    }
+    if (!noteId) {
+        return res.status(200).send({
+            message: "Error! Please provide a valid note id.",
+            noteId: noteId
+        });
+    }
+    
     
 })
 
@@ -129,17 +176,24 @@ router.post('/', authenticateToken, async (req, res) => {
 router.delete('/:noteId', authenticateToken, async (req, res) => {
     const { userId } = req.user
     const {noteId} = req.params;
-    try {
-        const result = await Notes.updateOne(
-            { _id: userId },
-            { $pull: { notes: {_id: noteId } } }
-        );
-        return res.status(200).send({message: "Successfully deleted note."});
-    } catch (error) {
-        return res.status(500).send({
-            message: "Failed to delete note."
-        });
+    if (noteId) {
+        try {
+            const result = await Notes.updateOne(
+                { _id: userId },
+                { $pull: { notes: {_id: noteId } } }
+            );
+            return res.status(200).send({message: "Successfully deleted note."});
+        } catch (error) {
+            return res.status(500).send({
+                message: "Failed to delete note."
+            });
+        }
     }
+    return res.status(400).send({
+        message: "Error! Please provide a valid note ID.",
+        noteId: ''
+    });
+    
     
 });
 
