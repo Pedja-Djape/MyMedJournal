addaeb0c37168ec28db11d94f72e2abf9ae35d82
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-16 13:48:44 -0400

    note rendering Dashboard if not authenticated

--- client/src/pages/DashboardLayout.js
+++ client/src/pages/DashboardLayout.js
@@ -1,15 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Await, Navigate } from "react-router-dom";
 
-import React from 'react'
+import React, { Suspense } from 'react'
 import DashboardNavigation from "../components/DashboardNavigation";
+import { useSelector } from "react-redux";
 
 const DashboardLayout = () => {
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
     return (
     <>
-        <div className="flex gap-4">
-            <DashboardNavigation />
-            <Outlet />
-        </div>
+    <Suspense>
+        <Await resolve={isAuthenticated}>
+            {(resolveAuth => {
+                if (resolveAuth) {
+                    return (
+                        <div className="flex gap-4">
+                            <DashboardNavigation />
+                            <Outlet />
+                        </div>
+                    )
+                }
+                return <Navigate to='/auth?mode=login'/>
+            })}
+            
+        </Await>
+        
+        
+    </Suspense>
         
     </>
     )
