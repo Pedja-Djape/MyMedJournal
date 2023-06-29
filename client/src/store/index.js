3f944a6f2763a89ac9e86cdf30996a02a4e7da66
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-29 15:53:10 -0400

    turnoff dev tools while in prod

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,7 +5,8 @@ import authSlice from "./authSlice";
 const store = configureStore({
     reducer: {
         auth: authSlice.reducer
-    }
+    },
+    devTools: process.env.NODE_ENV !== 'production',
 });
 
 export default store;
