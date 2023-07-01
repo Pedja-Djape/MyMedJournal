433add70f237474dc1526d7e8421705f2db8375f
Pedja-Djape <pmuratov1015@gmail.com>
2023-06-30 21:04:53 -0400

    Wrapped App with PersistGate to wait to render on hydration

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css';
 import App from './App';
-import store from './store'
+import {store, persistor } from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-       <Provider store={store}><App /></Provider> 
+       <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
+       </Provider> 
   </React.StrictMode>
 );
 
